b80ac3bb16540f2d331be620444d87c9cc887577
mosco04 <92190675+mosco04@users.noreply.github.com>
2021-10-09 16:59:41 +0100

    added routes to index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+// About me roue
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+// Projects route
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'project' });
+});
+
+// Contact me route
+router.get('/contactme', function(req, res, next) {
+  res.render('contactme', { title: 'contactme' });
+});
+
+// Services route
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'services' });
+});
+
+
 
 module.exports = router;
