22ea1d36fc3d2e98defd24961b5198134af30cfb
mosco04 <kolejess17@gmail.com>
2021-10-23 17:40:22 +0100

    added redirecting to business when logged out

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const db = new JSONdb(__dirname + '..\database\database.json');
 // hardcode username and password
 db.set("username", "edward",)
 db.set("password", "edward_007",)
+db.set('isLoggedIn', false)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,36 +18,50 @@ router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-// About me roue
+// About me route
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
 
-// Auth roue
+// Business route
+router.get('/business', function(req, res, next) {
+  if(db.get('isLoggedIn')){
+    res.render('business', { title: 'Business' });
+  }else{
+    res.redirect('/login') 
+    // ('login', { title: 'Login' });
+  }
+});
+// Auth route
 router.get('/login', function(req, res, next) {
-  console.log(db.get('username'))
   res.render('login', { title: 'Login' });
 });
 
+//Auth result
 router.post('/login', function(req, res, next) {
-  // res.render('login', { title: 'Login' });
-  console.log(req.body.email)
-  console.log(req.body.password)
+  // check username and password
+  ((req.body.username === db.get('username')) &&
+  (req.body.password === db.get('password')))  ?
+  res.redirect('/business'): 
+  res.render('login', { title: 'Wrong' }) 
+
+  // res.redirect('/login')
+
 });
 
 // Projects route
 router.get('/project', function(req, res, next) {
-  res.render('project', { title: 'project' });
+  res.render('project', { title: 'Project' });
 });
 
 // Contact me route
 router.get('/contactme', function(req, res, next) {
-  res.render('contactme', { title: 'contactme' });
+  res.render('contactme', { title: 'Contactme' });
 });
 
 // Services route
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'services' });
+  res.render('services', { title: 'Services' });
 });
 
 
