0ac0ed6575e317e97c5918c86f940d2e03bbeac4
mosco04 <kolejess17@gmail.com>
2021-10-23 16:55:20 +0100

    added authentication json database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
+const { body,validationResult } = require('express-validator');
+const JSONdb = require('simple-json-db');
+const db = new JSONdb(__dirname + '..\database\database.json');
+// hardcode username and password
+db.set("username", "edward",)
+db.set("password", "edward_007",)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,7 +24,14 @@ router.get('/about', function(req, res, next) {
 
 // Auth roue
 router.get('/login', function(req, res, next) {
+  console.log(db.get('username'))
   res.render('login', { title: 'Login' });
+});
+
+router.post('/login', function(req, res, next) {
+  // res.render('login', { title: 'Login' });
+  console.log(req.body.email)
+  console.log(req.body.password)
 });
 
 // Projects route
